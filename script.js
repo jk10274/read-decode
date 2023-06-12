b8aa0a79d04d0bf8924cb3a18d7e3f95cc9895a5
jk <72466440+jk10274@users.noreply.github.com>
2023-06-12 02:03:48 +0200

    Adjust generation data

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ fetch('texts.json')
   .then(data => {
     const imageContainer = document.getElementById('image-container');
 
+    // Get the maximum "Time" value to calculate the delay
+    let maxTime = 0;
+    for (let text in data.Texts) {
+      data.Texts[text].forEach(imageInfo => {
+        if (imageInfo.Time > maxTime) {
+          maxTime = imageInfo.Time;
+        }
+      });
+    }
+
     // Loop through each object in the Texts object
     for (let text in data.Texts) {
       // Loop through each subobject (in case there are multiple images per text)
@@ -30,6 +40,17 @@ fetch('texts.json')
         img.style.top = `${top}px`;
         img.style.left = `${left}vw`;
         img.style.zIndex = 1000;
+
+        // Initially set opacity to 0
+        img.style.opacity = 0;
+
+        // Add the fade-in class to the image
+        img.classList.add('fade-in');
+
+        // Set the animation delay based on the "Time" value
+        let delay = (imageInfo.Time / maxTime) * 1; // The multiplier controls the total duration
+        img.style.animationDelay = `${delay}s`;
+
         // Append the image to the imageContainer
         imageContainer.appendChild(img);
       });
