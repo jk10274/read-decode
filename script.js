d7aaeb0ee17a92eac5a912182db03b03b558a85a
jk <72466440+jk10274@users.noreply.github.com>
2023-06-13 13:52:46 +0200

    Working Categories

--- script.js
+++ script.js
@@ -36,6 +36,12 @@ fetch("texts.json")
 					// Set the title of the image to the object's title
 					img.title = text;
 
+          // Set the data-category attribute of the div
+          img.setAttribute('data-category', imageInfo.Category);
+
+          // Add the 'image' class to the div
+          img.classList.add('image');
+
 					// Set the size of the image based on the "Font-Size" value
 					img.style.height = `${imageInfo["Size"] * 10}px`;
 					img.style.width = "auto";
@@ -84,6 +90,7 @@ fetch("texts.json")
 		textmengeButton.addEventListener("click", () => {
 			// Change the text on the timeline
 			timelineTextLeft.textContent = "weniger";
+      timelineTextCenter.textContent = "";
 			timelineTextRight.textContent = "mehr";
 
 			// Place the images based on "Amount"
@@ -94,20 +101,65 @@ fetch("texts.json")
 		relevanzButton.addEventListener("click", () => {
 			// Change the text on the timeline
 			timelineTextLeft.textContent = "weniger wichtig";
+      timelineTextCenter.textContent = "";
 			timelineTextRight.textContent = "wichtiger";
 
 			// Place the images based on "Importance"
 			placeImages(data, "Importance");
 		});
 
+		contextButton.addEventListener("click", () => {
+			// Change the text on the timeline
+			timelineTextLeft.textContent = "Privat";
+			timelineTextCenter.textContent = "Schulisch";
+			timelineTextRight.textContent = "Freizeit";
+
+			// Place the images based on "Context"
+			placeImages(data, "Context");
+		});
+
 		schriftgroesseButton.addEventListener("click", () => {
 			// Change the text on the timeline
 			timelineTextLeft.textContent = "kleiner";
+      timelineTextCenter.textContent = "";
 			timelineTextRight.textContent = "größer";
 
 			// Place the images based on "Font-Size"
 			placeImages(data, "Font-Size");
 		});
+
+		farbeButton.addEventListener("click", () => {
+			// Change the text on the timeline
+			timelineTextLeft.textContent = "Schwarz";
+			timelineTextCenter.textContent = "Weiss";
+			timelineTextRight.textContent = "Bunt";
+
+			// Place the images based on "Color"
+			placeImages(data, "Color");
+		});
+
+		situationButton.addEventListener("click", () => {
+			// Change the text on the timeline
+			timelineTextLeft.textContent = "Zuhause";
+      timelineTextCenter.textContent = "";
+			timelineTextRight.textContent = "Unterwegs";
+
+			// Place the images based on "Situation"
+			placeImages(data, "Situation");
+		});
+
+    document.getElementById('category').addEventListener('change', function() {
+      var category = this.value;
+      var images = document.getElementsByClassName('image');
+    
+      for (var i = 0; i < images.length; i++) {
+        if (category === 'alle' || images[i].getAttribute('data-category') === category) {
+          images[i].style.display = 'block';
+        } else {
+          images[i].style.display = 'none';
+        }
+      }
+    });
 	})
 	.catch((error) => console.error("Error:", error));
 
